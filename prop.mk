f968c53e37f63ccd3ec510c46766abe837655a61
Albert I <krascgq@outlook.co.id>
2017-09-17 12:47:52 +0800

    msm8953-common: dex2oat filter = speed
    Change-Id: Iee0f0f524490c813e43ff18fc5a327a119114aa0
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- prop.mk
+++ prop.mk
@@ -2,6 +2,11 @@
 # system.prop for msm8953-common
 #
 
+# ART
+PRODUCT_PROPERTY_OVERRIDES += \
+dalvik.vm.dex2oat-filter=speed \
+dalvik.vm.image-dex2oat-filter=speed
+
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
 af.fast_track_multiplier=2 \
