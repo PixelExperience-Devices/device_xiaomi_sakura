d2efc2da6448f064f73052d7d6e2cc938c05995c
Suren Baghdasaryan <surenb@google.com>
2018-01-09 15:47:40 -0800

    sakura: Set lmkd configuration properties
    Bug: 63631020
    
    Change-Id: I16a183e8e4625e8b98cefdc9c8a7b650556a2b55
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- prop.mk
+++ prop.mk
@@ -173,6 +173,17 @@ ro.hwui.text_small_cache_height=1024 \
 ro.hwui.text_large_cache_width=2048 \
 ro.hwui.text_large_cache_height=1024
 
+# LMKD
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.lmk.low=1001 \
+ro.lmk.medium=0 \
+ro.lmk.critical=0 \
+ro.lmk.critical_upgrade=false \
+ro.lmk.upgrade_pressure=100 \
+ro.lmk.downgrade_pressure=100 \
+ro.lmk.kill_heaviest_task=true \
+ro.lmk.kill_timeout_ms=100
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
 av.debug.disable.pers.cache=1 \
