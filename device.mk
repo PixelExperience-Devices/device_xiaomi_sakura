4adee244afbe0403c030443103ee8e09afb5a98b
mdeejay <mdjrussia@gmail.com>
2019-01-04 13:07:12 +0100

    sakura: Enable QCOM Boost framework
    * Thanks to @kondors1995 for idea

--- device.mk
+++ device.mk
@@ -273,6 +273,11 @@ PRODUCT_COPY_FILES += \
 # QMI
 PRODUCT_PACKAGES += \
     libjson
+	
+#QTI performance
+PRODUCT_BOOT_JARS += \
+    QPerformance \
+    UxPerformance
 
 # Ramdisk
 PRODUCT_PACKAGES += \
--- prop.mk
+++ prop.mk
@@ -146,6 +146,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ro.vendor.extension_library=libqti-perfd-client.so \
 ro.vendor.qti.sys.fw.bservice_enable=true
 
+#QTI Performance
+PRODUCT_PROPERTY_OVERRIDES += \
+vendor.enable_prefetch=1 \
+vendor.iop.enable_uxe=1 \
+vendor.iop.enable_prefetch_ofr=1 \
+vendor.perf.iop_v3.enable=1 \
+ro.vendor.at_library=libqti-at.so \
+persist.vendor.qti.games.gt.prof=1
+
 # Netmgrd
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.vendor.use_data_netmgrd=true \
