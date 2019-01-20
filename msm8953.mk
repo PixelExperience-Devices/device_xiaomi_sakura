f15d104adca92d32e4db2a074a35590651aba71a
Rama Bondan Prakoso <ramanarubp@gmail.com>
2019-01-20 12:58:09 +0700

    msm8953-common: Update Camera Packages

--- msm8953.mk
+++ msm8953.mk
@@ -112,8 +112,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    camera.msm8953 \
-    libmm-qcamera \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor \
     Snap
 
 # Configstore
