2106107441ac649fe868a53f03be77de60bbf039
Rama Bondan Prakoso <ramanarubp@gmail.com>
2019-01-20 12:58:09 +0700

    sakura: Update Camera Packages

--- device.mk
+++ device.mk
@@ -119,8 +119,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    camera.msm8953 \
-    libmm-qcamera \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor \
     Snap
 
 # Configstore
