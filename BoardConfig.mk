4685cbfcc7a9783aeb56fbf87a12d6940c6b967e
Rama Bondan Prakoso <ramanarubp@gmail.com>
2019-01-23 13:27:18 +0700

    sakura: Update DT2W node and Add Permission

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,7 +181,7 @@ TARGET_PER_MGR_ENABLED := true
 
 # Power
 TARGET_HAS_NO_WLAN_STATS := true
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
+TARGET_TAP_TO_WAKE_NODE := "/proc/tp_wakeup_gesture"
 TARGET_USES_INTERACTION_BOOST := true
 
 # Qualcomm
