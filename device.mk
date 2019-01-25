1bf48af3f77a75b39e55a5d3aafcaac153feefe1
Revanth <princechalapathi16@gmail.com>
2019-01-25 14:03:45 +0000

    sakura: Drop camera,biometrics,consumerir Hal

--- device.mk
+++ device.mk
@@ -127,10 +127,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
 
-# Consumerir
-PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-service.xiaomi_sakura
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
@@ -165,10 +161,6 @@ PRODUCT_PACKAGES += \
     ebtables \
     ethertypes \
     libebtc
-
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sakura
 
 # FM
 PRODUCT_PACKAGES += \
