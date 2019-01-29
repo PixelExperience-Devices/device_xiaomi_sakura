639bb6968ddc06a569b4d805cf219399423bc68a
Revanth <princechalapathi16@gmail.com>
2019-01-29 09:42:42 +0530

    sakura: Inherit MIUI Camera & Fix Derp

--- lineage_sakura.mk
+++ lineage_sakura.mk
@@ -17,6 +17,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/sakura.mk)
 
 # Inherit from sakura device
 $(call inherit-product, device/xiaomi/sakura/device.mk)
