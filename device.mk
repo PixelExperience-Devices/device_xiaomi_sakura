8e25ce9d6420b98d87da8d3d2f17d21b2168e3ec
Revanth <princechalapathi16@gmail.com>
2019-02-03 11:17:25 +0000

    sakura: Lets Use Qcom Power Hal

--- device.mk
+++ device.mk
@@ -266,7 +266,11 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+     power.qcom
+
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
