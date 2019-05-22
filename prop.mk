c76ef5a22f2e7c8ca71bb5bb1ad32b816682917f
Devil7DK <dineshthangavel47@gmail.com>
2019-05-22 18:12:52 +0000

    sakura: Upate props from sakura pie - PKQ1.180917.001

--- prop.mk
+++ prop.mk
@@ -55,7 +55,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 bluetooth.hfp.client=1 \
 vendor.qcom.bluetooth.soc=smd \
 ro.bluetooth.hfp.ver=1.7 \
-ro.qualcomm.bt.hci_transport=smd
+ro.qualcomm.bt.hci_transport=smd \
+ro.bluetooth.library_name=libbluetooth_qti.so \
+persist.vendor.btstack.enable.splita2dp=false
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -65,13 +67,17 @@ camera.lowpower.record.enable=1 \
 media.camera.ts.monotonic=1 \
 persist.camera.CDS=off \
 persist.camera.video.CDS=off \
+persist.camera.eis.enable=1 \
 persist.camera.dual.camera=0 \
 persist.camera.gyro.disable=0 \
 persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
 persist.vendor.qti.telephony.vt_cam_interface=1 \
 vidc.enc.dcvs.extra-buff-count=2 \
-persist.camera.HAL3.enabled=1
+persist.camera.HAL3.enabled=1 \
+vendor.camera.lowpower.record.enable=1 \
+vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.huaqin.factory,org.lineageos.snap \
+vendor.camera.aux.packagelist2=com.android.systemui,com.huaqin.cameraautotest,com.huaqin.runtime \
 
 # Cne
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -110,7 +116,12 @@ sdm.debug.disable_skip_validate=1 \
 vendor.display.enable_default_color_mode=1 \
 vendor.display.disable_skip_validate=1 \
 vendor.display.enable_default_color_mode=1 \
-vendor.gralloc.enable_fb_ubwc=1
+vendor.gralloc.enable_fb_ubwc=1 \
+persist.vendor.max.brightness=475
+
+# DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.dpm.feature=1
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -134,6 +145,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
 persist.gps.qc_nlp_in_use=1 \
 persist.loc.nlp_name=com.qualcomm.location \
 ro.gps.agps_provider=1
+
+# HWUI
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.hwui.texture_cache_size=72 \
+ro.hwui.layer_cache_size=48 \
+ro.hwui.r_buffer_cache_size=8 \
+ro.hwui.path_cache_size=32 \
+ro.hwui.gradient_cache_size=1 \
+ro.hwui.drop_shadow_cache_size=6 \
+ro.hwui.texture_cache_flushrate=0.4 \
+ro.hwui.text_small_cache_width=1024 \
+ro.hwui.text_small_cache_height=1024 \
+ro.hwui.text_large_cache_width=2048 \
+ro.hwui.text_large_cache_height=1024
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -211,7 +236,9 @@ rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
 ro.telephony.call_ring.multiple=false \
 ro.telephony.default_network=22,20 \
 service.qti.ims.enabled=1 \
-telephony.lteOnCdmaDevice=1
+telephony.lteOnCdmaDevice=1 \
+persist.vendor.radio.data_con_rprt=1
+
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
