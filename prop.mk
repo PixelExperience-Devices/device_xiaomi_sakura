a3bbfb7095266bc5d77750eb5f08072052d56434
ZIDAN44 <tuhinsultana075@gmail.com>
2019-06-10 07:55:50 +0000

    sakura: cleanup GPS properties

--- prop.mk
+++ prop.mk
@@ -139,12 +139,6 @@ vendor.hw.fm.init=0
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.frp.pst=/dev/block/bootdevice/by-name/config
 
-# GPS
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.gps.qc_nlp_in_use=1 \
-persist.loc.nlp_name=com.qualcomm.location \
-ro.gps.agps_provider=1
-
 # HWUI
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.hwui.texture_cache_size=72 \
