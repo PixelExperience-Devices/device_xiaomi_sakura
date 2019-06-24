ac192c10cb5f853e6194d6315d2c7dd01a7efabf
Bladeyy <getsuga369@gmail.com>
2019-06-24 18:51:50 +0530

    sakura: Enable USB debugging at boot
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- prop.mk
+++ prop.mk
@@ -290,6 +290,13 @@ sys.use_fifo_ui=1
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.vendor.usb.config.extra=none
 
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.sys.usb.config=mtp,adb \
+ro.adb.secure=0 \
+ro.secure=0 \
+ro.debuggable=1
+
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
 wifi.interface=wlan0
