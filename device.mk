218f2ccf75fb7c0d568e384ea02aa3c51578e03c
Isaac Chen <tingyi364@gmail.com>
2019-07-15 07:16:47 +0200

    sakura: Import XiaomiParts
    * From https://github.com/LineageOS/android_device_xiaomi_sdm845-common/commit/14835c7dddb2140b4c935586ebfb776fe6f738c1
    
    Signed-off-by: Zinadin Zidan <tuhinsultana075@gmail.com>

--- device.mk
+++ device.mk
@@ -159,6 +159,10 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.9 \
     vendor.display.config@1.9_vendor
 
+# Device Specific Settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
