91f6c9bcf140dae29f60c70e9fabe38ca806e389
Dyneteve <sound0020@gmail.com>
2019-07-21 19:43:48 +0300

    sakura: Build libaudioroute

--- device.mk
+++ device.mk
@@ -86,6 +86,7 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8953 \
     audio.r_submix.default \
     audio.usb.default \
+    libaudioroute \
     libaacwrapper \
     libaudio-resampler \
     libqcomvisualizer \
