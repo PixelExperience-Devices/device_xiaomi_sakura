5b89e3ec0be0c196807bfc11e8e1d411e5fb3e8f
Erfan Abdi <erfangplus@gmail.com>
2019-09-11 17:36:48 +0600

    sakura: Add updatable_apex.mk
    Signed-off-by: Zinadin Zidan <tuhinsultana075@gmail.com>

--- aosp_sakura.mk
+++ aosp_sakura.mk
@@ -8,6 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Inherit from sakura device
 $(call inherit-product, device/xiaomi/sakura/device.mk)
