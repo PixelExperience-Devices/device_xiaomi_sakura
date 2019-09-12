99e394bdc95f9186bc4540657ccea5fa99bb28d2
RevanthTolety <crevanthtolety@gmail.com>
2019-09-12 07:16:26 +0000

    msm8953-common: Add Support For Redmi 6 Pro

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter mido tissot,$(TARGET_DEVICE)),)
+ifneq ($(filter sakura,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
