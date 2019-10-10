a84e97bb5eb61fb6e9580d548ca8b0e641ac1115
Ryan Mitchell <rtmitchell@google.com>
2019-10-10 08:09:29 -0700

    msm8953-common: Use component-override to configure Bluetooth
    Runtime Resource Overlays (RROs) can no longer change the value of
    resources in manifest when read during PackagerParser.
    
    This change uses component-override to enable the service by default.
    
    Remove usages of profile_supported_* from overlays as they no
    longer configure the enable state of Bluetooth components.
    
    Bug: 135048762
    Test: adb shell pm query-services \
          com.android.bluetooth/.avrcpcontroller.AvrcpControllerService
          adb shell pm query-services \
          com.android.bluetooth/.hfpclient.HeadsetClientService
          adb shell pm query-services \
          com.android.bluetooth/.sap.SapService
    
    Change-Id: I284d254a921b14d1533745611a083f4b5a27d9dc
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -119,6 +119,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
 
+# Component overrides
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
 # Dex
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI \
