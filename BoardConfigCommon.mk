72dc6b5f4a6158606f9a010b0da2302ed497acf7
mahajant99 <mahajant99@gmail.com>
2019-11-03 02:01:46 +0530

    msm8953-common: Switch to qcom legacy sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -183,7 +183,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/fstab.qcom
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # We modify several neverallows, so let the build proceed
