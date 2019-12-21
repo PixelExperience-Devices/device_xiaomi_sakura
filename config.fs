2b32d896dc42f990ab21d69df9ac7c64577c2c9a
Brandon Solis <bsolis@codeaurora.org>
2019-12-21 12:56:33 +0100

    sakura: Update User For IMS Daemons
    Change user for imsdatadaemon, imsrcsd,
    ims_rtp_daemon
    from AID_SYSTEM to AID_RADIO
    
    Change-Id: I8ee1c4ff182af0d9b4ebf028e3096cfa88118287
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config.fs
+++ config.fs
@@ -30,8 +30,8 @@ caps: NET_BIND_SERVICE
 
 [vendor/bin/pd-mapper]
 mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
+user: AID_RADIO
+group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [vendor/bin/imsdatadaemon]
