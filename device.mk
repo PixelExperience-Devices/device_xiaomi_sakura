1b3716043bb14ba253f44730217499ecf1bac24e
ZIDAN44 <tuhinsultana075@gmail.com>
2020-01-14 16:00:22 +0600

    sakura: Import init.recovery.qcom
    * Imported From xiaomi/sakura_india/sakura_india:9/PKQ1.180917.001/V11.0.5.0.PDMMIXM:user/release-keys

--- device.mk
+++ device.mk
@@ -314,6 +314,7 @@ PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
+    init.recovery.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.target.rc \
