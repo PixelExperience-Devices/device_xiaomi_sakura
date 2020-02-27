0f87c19ff7bb3d07118e66fb471cd4dc2a89138e
zeelog <zeelog>
2020-02-27 23:31:45 +0530

    sakura: Prefer HW codec over SW for thumbnail
    Change-Id: I2f33a7ce5b14ea9be7ea73714ea32f7b80806da2

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -175,6 +175,7 @@ debug.sf.enable_hwc_vds=1 \
 media.aac_51_output_enabled=true \
 media.msm8956hw=0 \
 media.stagefright.audio.sink=280 \
+media.stagefright.thumbnail.prefer_hw_codecs=true \
 vendor.mm.enable.qcom_parser=1048575 \
 mm.enable.smoothstreaming=true \
 mmp.enable.3g2=true \
