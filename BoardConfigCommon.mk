327d39f161431ea0f2ae27aab864dea32acae7ac
mahajant99 <mahajant99@gmail.com>
2020-05-10 12:46:36 +0530

    msm8953-common: Nuke Ignore Selinux Neverallows
    This reverts commit 3bcdd3762285a207462bc9e8cc6ee6e97b697225.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -187,11 +187,6 @@ TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/fstab.qcom
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
-# We modify several neverallows, so let the build proceed
-ifneq ($(TARGET_BUILD_VARIANT),user)
-SELINUX_IGNORE_NEVERALLOWS := true
-endif
-
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HOSTAPD_DRIVER := NL80211
