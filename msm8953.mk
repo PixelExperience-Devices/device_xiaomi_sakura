af0acec40a8f7220a04858e1e8122c8223f2e5c4
mahajant99 <mahajant99@gmail.com>
2020-05-11 20:46:04 +0530

    msm8953-common: Remove Safailnet
    This reverts commit 38fb5252eec8497cf6cd8f1ce2cf6c3437753cdd.

--- msm8953.mk
+++ msm8953.mk
@@ -277,8 +277,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.target.rc \
-    ueventd.qcom.rc \
-    init.safailnet.rc
+    ueventd.qcom.rc
 
 PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
