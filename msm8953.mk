35c9358fb76750a849ba5e64bbde4423776a027f
Davide Garberi <dade.garberi@gmail.com>
2020-09-10 03:57:15 +0200

    msm8953-common: Build libhidltransport and libhwbinder
    Change-Id: I1b4b255ce40c94e3bf2e6234b7280fc094a9270b

--- msm8953.mk
+++ msm8953.mk
@@ -185,7 +185,9 @@ PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    libhidltransport \
+    libhwbinder
 
 # HW crypto
 PRODUCT_PACKAGES += \
