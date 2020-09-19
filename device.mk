70c1267dcd94b5981bd823f53dcd5e9ce166b80f
Zinadin Zidan <zidan44@pixelexperience.org>
2020-09-20 03:00:11 +0600

    sakura: Prebuild Some Camera Blobs From Stock
    * Source Build Camera Blobs Are Broken Now.
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I4578af4cf03a37f93d5922c975ba91598ddf3e40

--- device.mk
+++ device.mk
@@ -143,13 +143,13 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     android.hardware.camera.device@3.4 \
-    vendor.qti.hardware.camera.device@1.0 \
-    libdng_sdk.vendor \
-    libgui_vendor
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    libdng_sdk.vendor
 
 # Codec2
 PRODUCT_PACKAGES += \
