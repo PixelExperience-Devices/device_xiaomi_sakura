55bfcb4012663e2b78373a5602257a756a1f0a9c
Zinadin Zidan <zidan44@pixelexperience.org>
2020-09-19 22:49:01 +0600

    sakura: Build configstore 1.1 HAL
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I04620b5afbd4f3c37510a7c50dc8e0d901cf31e8

--- device.mk
+++ device.mk
@@ -169,7 +169,7 @@ PRODUCT_PACKAGES += \
 
 # Configstore
 PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.0-service
+    android.hardware.configstore@1.1-service
 
 # Camera
 PRODUCT_PACKAGES += \
