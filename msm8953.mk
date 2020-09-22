96959a75ca74235ca6fbd182daebedc639859034
LuK1337 <priv.luk@gmail.com>
2020-09-22 10:15:09 +0200

    msm8953-common: Enforce vintf manifest override
    * Apparently this is now required on all non full treble devices since
      google added a sleep(1) in libhidl...
    
    Change-Id: I6d8492dd7c56bfdb2ca00f3477634a46fa55b1c0

--- msm8953.mk
+++ msm8953.mk
@@ -181,6 +181,8 @@ PRODUCT_PACKAGES += \
     chargeonlymode
 
 # HIDL
+PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
+
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
