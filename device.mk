83629b524867fcc252021e4a2c81f8e2cd874993
Zinadin Zidan <zidan44@pixelexperience.org>
2020-12-03 11:42:57 +0600

    sakura: light: Rename Lighting Hals To Sakura
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Iee41c014d077ce6e8ee95ab150bda4eb273a32b5

--- device.mk
+++ device.mk
@@ -261,7 +261,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.msm8953
+    android.hardware.light@2.0-service.sakura
 
 # Media
 PRODUCT_COPY_FILES += \
--- light/service.cpp
+++ light/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.light@2.0-service.msm8953"
+#define LOG_TAG "android.hardware.light@2.0-service.sakura"
 
 #include <hidl/HidlTransportSupport.h>
 
