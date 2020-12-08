d7e6cb111da49146c901cd6e8e0731de0f8b15e1
Bruno Martins <bgcngm@gmail.com>
2020-12-08 22:03:48 +0000

    sakura: Build hardware health 2.1 HALs
    Change-Id: Ia13d872d4f03dd08ccd63428397b4c7d6810cb9b

--- device.mk
+++ device.mk
@@ -201,8 +201,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # Healthd
 PRODUCT_PACKAGES += \
