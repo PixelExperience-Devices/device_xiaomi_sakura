47a51d3995d8e8ff6b9858a78f65cea22facb966
dianlujitao <dianlujitao@lineageos.org>
2021-03-11 16:25:05 +0600

    sakura: build vendor qti perf interfaces from source
    For some reason, any prebuilt ones from pre-R ROMs no longer work on R
    and cause the following error:
      E ANDR-PERF: Unable to link to gPerfHal death notifications!
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I088e95205ac1f2c0679d9d4c069d3e0434a71df0

--- device.mk
+++ device.mk
@@ -287,6 +287,13 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@1.0.vendor \
+    vendor.qti.hardware.perf@2.0.vendor \
+    vendor.qti.hardware.perf@2.1.vendor \
+    vendor.qti.hardware.perf@2.2.vendor
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
