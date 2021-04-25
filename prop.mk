d8e51a844d47fbcdab741143af5387e29137cc34
Zinadin Zidan <zidan44@pixelexperience.org>
2021-04-25 11:28:46 +0600

    sakura: prop: Add custom netflix property
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I70b2ce2de9f8d06e26c3f0766a86dcfd31564ebd

--- prop.mk
+++ prop.mk
@@ -156,6 +156,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ro.sys.fw.dex2oat_thread_count=8 \
 ro.vendor.extension_library=libqti-perfd-client.so
 
+# Netflix custom property
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.netflix.bsp_rev=Q6150-17263-1
+
 # Netmgrd
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.vendor.use_data_netmgrd=true \
