d32fcd334022f84d4465927cb6698c1a1bbaceca
Quallenauge <Hamsi2k@freenet.de>
2021-04-25 13:43:52 +0600

    sakura: overlay: Exclude Snap overlays from RRO
    * Snap overlays don't work with RRO
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: If2e978971be70cae7bdd46c68dfcb509f5cddafc

--- device.mk
+++ device.mk
@@ -31,6 +31,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # RRO (Runtime Resource Overlay)
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 # Board
 PRODUCT_USES_QCOM_HARDWARE := true
