ed6efef6f00a88009bd20d373c5a2440cf8e0e4b
ItsVixano <vixanonew@gmail.com>
2021-10-08 16:57:27 +0200

    sakura: camera: Drop HALv1 once and for for all
    * Has been deprecated on aosp-12
    
    Change-Id: I02d5f320db9243c04d89d58fef775dfe33fe4ac7

--- prop.mk
+++ prop.mk
@@ -64,6 +64,7 @@ persist.vendor.camera.isp.clock.optmz=0 \
 persist.vendor.camera.stats.test=5 \
 persist.vendor.camera.CDS=off \
 persist.vendor.camera.aux.camera=1 \
+persist.vendor.camera.HAL3.enabled=1 \
 persist.camera.HAL3.enabled=1
 
 # Charger
