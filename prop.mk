2f2d5e9aaadc07602daa6447bcf316c25e983ac8
ItsVixano <vixanonew@gmail.com>
2021-10-21 15:57:41 +0200

    sakura: prop: Expose aux camera prop
    Change-Id: Ifba56235a2db38c492fbb798541f4cc8e5c09fb3
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>

--- prop.mk
+++ prop.mk
@@ -63,6 +63,7 @@ persist.vendor.camera.gyro.disable=0 \
 persist.vendor.camera.isp.clock.optmz=0 \
 persist.vendor.camera.stats.test=5 \
 persist.vendor.camera.CDS=off \
+persist.vendor.camera.aux.camera=1 \
 persist.camera.HAL3.enabled=1
 
 # Charger
