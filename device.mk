5f5ff38981a3c4694846ae2bb3b461579d02b626
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-01-01 22:22:46 +0600

    sakura: Switch to AOSP WFD service
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I13d34661fa7184d9684bd28acbc4f1a137aa0336

--- device.mk
+++ device.mk
@@ -442,7 +442,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
-
-# Wi-Fi Display
-PRODUCT_BOOT_JARS += \
-    WfdCommon
