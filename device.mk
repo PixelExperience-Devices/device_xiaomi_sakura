332936e3e1d429060945d195c66a6a49382df064
Bruno Martins <bgcngm@gmail.com>
2022-05-30 19:27:51 +0100

    sakura: Drop default DRM service
    This is not needed, since Widevine DRM and Clearkey DRM
    services are available.
    
    Change-Id: I97023f1bab88ddb8fa4cb205df190c516a759951

--- device.mk
+++ device.mk
@@ -194,8 +194,6 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl:64 \
-    android.hardware.drm@1.0-service-lazy \
     android.hardware.drm@1.3-service.clearkey
 
 PRODUCT_PACKAGES += \
